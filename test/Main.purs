8534f3791457a5f19af8cc9d1cac5ea3b348be7f
Armand Adroher <aadroher@gmail.com>
2018-05-10 08:48:45 +0100

    Failed attempts at using quickcheck

--- test/Main.purs
+++ test/Main.purs
@@ -4,8 +4,9 @@ import Prelude
 import Control.Monad.Eff (Eff)
 import Node.FS (FS)
 import Test.Spec.Discovery (discover)
+import Test.Spec.QuickCheck (QCRunnerEffects, quickCheck)
 import Test.Spec.Reporter.Console (consoleReporter)
 import Test.Spec.Runner (RunnerEffects, run)
 
-main :: Eff (RunnerEffects (fs :: FS)) Unit
+main :: Eff (QCRunnerEffects (fs :: FS)) Unit
 main = discover "PureLife\\.WorldSpec" >>= run [consoleReporter]
--- test/WorldSpec.purs
+++ test/WorldSpec.purs
@@ -2,16 +2,27 @@ module PureLife.WorldSpec where
 
 import Prelude
 -- import Control.Monad.Aff (later')
+import Control.Monad.Aff (Aff)
 import Control.Monad.Eff (Eff)
+import Control.Monad.Eff.Random    (RANDOM)
+import Test.QuickCheck ((===), (/==))
 import Test.Spec (Spec, pending, describe, it)
 import Test.Spec.Assertions (shouldEqual)
+import Test.Spec.QuickCheck (QCRunnerEffects, quickCheck)
 import Test.Spec.Reporter.Console (consoleReporter)
 import Test.Spec.Runner (RunnerEffects, run)
 
-spec :: forall r. Spec r Unit
+spec :: forall e. StateT Aff (random :: RANDOM | e) Unit
 spec =
     describe "The cell world" do
         it "does not do much" $ pure unit
+        it "should fail" do
+          shouldEqual true false
+        -- pending "not implemented yet"
+        it "can test with QucikCheck" do
+          quickCheck \n -> n + n === 2 * n
+        -- it "can fail with QucikCheck" do
+        --   quickCheck \n -> n + n === 3 * n
 
 
 -- main :: Eff (RunnerEffects ()) Unit
